03d2df80c455ed0164b6ca4638de0ab3eb6fa372
davidmmichaelson <davidmmichaelson@gmail.com>
2018-07-06 22:18:07 -0400

    commented joinsnips.py

--- joinsnips.py
+++ joinsnips.py
@@ -1,13 +1,17 @@
+# joinsnips.py # David Michaelson
 import os
+# script that joins all "snip_*" files into the TARGET_FILE, which
+# Atom expects to be "snippets.cson"
 
-TARGET_FILE = "snippets.cson"
+
+TARGET_FILE = "snippets.cson" # output file
 
 try:
     with open(TARGET_FILE, "w+") as join:
         print(f"Creating `{TARGET_FILE}`...\n") 
-        for _, _, files in os.walk(os.getcwd()):
+        for _, _, files in os.walk(os.getcwd()): # traverses all files in local dir
             for file in files:
-                if file.split('_')[0] == 'snip':
+                if file.split('_')[0] == 'snip': # and filters for 'snip_*'
                     with open(file) as f:
                         join.write(f.read() + '\n\n')
                         print(f"...Joined `{file}` to `{TARGET_FILE}`")
